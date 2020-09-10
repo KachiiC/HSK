3497f314a4858c141b0728c5a2c52747a3a63613
Kachii <Kacheokcheong@gmail.com>
2020-09-10 02:55:50 +0100

    change django settings and changed to fetch

--- backend/hsk_project/settings.py
+++ backend/hsk_project/settings.py
@@ -117,3 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
--- frontend/src/Pages/Levels/HSK1/index.js
+++ frontend/src/Pages/Levels/HSK1/index.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import {Table } from 'antd'
+import React, {useState, useEffect} from 'react'
 // Components
-import HSK1Data from './Table/HSK1Data'
+import {Table } from 'antd'
 import HSk1Columns from './Table/HSK1Columns'
 
 
 function HSK1 (){
 
+    const [hsk1Words, setHsk1Words] = useState([])
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/hsk_list/")
+        .then((response) => {
+            console.log("fetching") 
+            return response.json() 
+        })
+        .then((wordData) => { 
+            console.log("words being set")
+            setHsk1Words(wordData)
+        })
+        .catch((error) => { 
+            console.log("error!")
+            console.log(error)
+        })
+    })
+
     return (
         <>
             <h1 className="page-title">HSK1</h1>
-            <Table columns={HSk1Columns} dataSource={HSK1Data} className="hsk-tables"/>
+            <Table columns={HSk1Columns} dataSource={hsk1Words} className="hsk-tables"/>
         </>
     )
 }
